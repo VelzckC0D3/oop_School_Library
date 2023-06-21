f2254d76ceecbb13e400f2c60186434674520590
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-06-20 23:11:47 -0500

    minimal correction

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require './person'
 
 class Student < Person
   def initialize(age, classroom, _name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
